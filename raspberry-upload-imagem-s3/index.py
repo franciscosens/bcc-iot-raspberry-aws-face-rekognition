17248d0e5e40fce30900bf268714bab5dfd6292c
Francisco Lucas Sens <francisco.lucas.sens@gmail.com>
2019-02-21 17:24:26 -0300

    Desenvolvido o upload da imagem para amazon s3

--- raspberry-upload-imagem-s3/index.py
+++ raspberry-upload-imagem-s3/index.py
@@ -1,22 +1,42 @@
-import wget, boto3
+import wget, boto3, os
 
+def gerar_menu():
+  opcao = "0"
+  while(opcao != "8001"):
+    print()
+    print("|‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾|")
+    print("|                   MENU                   |")
+    print("|__________________________________________|")
+    print("| 1    Tirar foto e enviar para amazon     |")
+    print("|                                          |")
+    print("| 8001 Sair                                |")
+    print("|                                          |")
+    print("|__________________________________________|")
+    print()
+    opcao = input()
+    os.system('cls')
+    if(opcao == "1"):
+      obter_imagem_upload()
+    elif(opcao == "8001"):
+      print("Obrigado pro brincar com nosso software")
+    else:
+      print("Opção inválida jovem")
 
-url = "http://201.54.201.38:8080/?action=snapshot"
-name = wget.download(url, out="foto.jpg")
-print()
-print("obtida a foto: " + name)
+def obter_imagem_upload():
+  # url = "http://201.54.201.38:8080/?action=snapshot"
+  # nome = wget.download(url, out="foto.jpg")
+  # print("Foto " + nome + " resgatada com sucesso: ")
+  nome = "foto.jpg"
+  client = boto3.client(
+      's3',
+      aws_access_key_id='AKIAJWHY4NS3KZ2YO6NA',
+      aws_secret_access_key='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'
+  )
 
-client = boto3.client(
-  's3', 
-  aws_access_key_id = "AKIAIVZOZUKK27KAYUWA",
-  aws_secret_access_key = "jxxReSah+YthD9gkeENpUsOaurh8F+pEOe5E6zvI0"
-)
+  with open(nome, "rb") as data:
+    print("Iniciando o processo de upload da foto....")
+    client.upload_fileobj(data, "aws-rekognition-face", nome)
+    print("Foto enviada com sucesso para amazon s3")
 
-response = client.list_objects("aws-rekognition-face")
-print(response)
-
-# with open(name, "rb") as data:
-#   print(name)
-#   client.upload_fileobj(data, "aws-rekognition-face", 'mykey')
-#   print("enviada a foto para amazon s3")
-
+os.system("cls")
+gerar_menu()
